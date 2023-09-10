ad499e7c016df669bf1be30c817df19b3b65d07b
Denis Stanishevskiy <>
2023-09-11 02:27:02 +0300

    Actions to associate/unassociate file types with the app

--- QuickLookPreview/AppDelegate.swift
+++ QuickLookPreview/AppDelegate.swift
@@ -23,6 +23,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         showPreview(uris: filenames.map { NSURL(fileURLWithPath: $0) })
     }
 
+    @IBAction func associateWithFileTypes(_ sender: Any) {
+        let alert = NSAlert()
+        alert.messageText = NSLocalizedString("Do you really want to associate all supported file types with this application?", comment: "associate confirmation message")
+        alert.alertStyle = NSAlert.Style.critical
+        alert.addButton(withTitle: NSLocalizedString("Yes", comment: "yes"))
+        alert.addButton(withTitle: NSLocalizedString("No", comment: "no"))
+        if alert.runModal() == NSApplication.ModalResponse.alertFirstButtonReturn {
+            for type in supportedTypes {
+                associateWith(type: type)
+            }
+        }
+    }
+
+    @IBAction func unassociateWithFileTypes(_ sender: Any) {
+        let alert = NSAlert()
+        alert.messageText = NSLocalizedString("Do you really want to cancel association with supported file types?", comment: "unassociate confirmation message")
+        alert.alertStyle = NSAlert.Style.critical
+        alert.addButton(withTitle: NSLocalizedString("Yes", comment: "yes"))
+        alert.addButton(withTitle: NSLocalizedString("No", comment: "no"))
+        if alert.runModal() == NSApplication.ModalResponse.alertFirstButtonReturn {
+            for type in supportedTypes {
+                unassociateWith(type: type)
+            }
+        }
+    }
+
     private var previewUris = [NSURL]()
 }
 
@@ -60,3 +86,61 @@ extension AppDelegate: QLPreviewPanelDataSource {
         NSApplication.shared.terminate(nil)
     }
 }
+
+extension AppDelegate {
+    private func associateWith(type: String) {
+        guard let ownId = Bundle.main.bundleIdentifier else { return }
+
+        let current = LSCopyDefaultRoleHandlerForContentType(type as CFString, .viewer)?.takeRetainedValue() as? String
+        if let current, current.caseInsensitiveCompare(ownId) != .orderedSame, current != dummyId {
+            UserDefaults.standard.setValue(current, forKey: keyForSavedType(type))
+        }
+
+        LSSetDefaultRoleHandlerForContentType(type as CFString, .viewer, ownId as CFString)
+    }
+
+    private func unassociateWith(type: String) {
+        guard let ownId = Bundle.main.bundleIdentifier else { return }
+
+        guard let current = LSCopyDefaultRoleHandlerForContentType(type as CFString, .viewer)?.takeRetainedValue() as? String else { return }
+        guard current.caseInsensitiveCompare(ownId) == .orderedSame else { return }
+
+        let prevHandler = UserDefaults.standard.string(forKey: keyForSavedType(type)) ?? dummyId
+        LSSetDefaultRoleHandlerForContentType(type as CFString, .viewer, prevHandler as CFString)
+    }
+}
+
+private func keyForSavedType(_ type: String) -> String { "saved." + type }
+
+private let dummyId = ""
+private let supportedTypes = [
+    "com.adobe.pdf",
+    "public.jpeg",
+    "public.png",
+    "com.microsoft.bmp",
+    "com.compuserve.gif",
+    "com.adobe.photoshop-image",
+    "public.mpeg",
+    "public.mpeg-2-video",
+    "public.mpeg-4",
+    "public.avi",
+
+    "com.microsoft.waveform-audio",
+    "public.mp3",
+    "public.mpeg-4-audio",
+
+    "public.plain-text",
+    "public.rtf",
+
+    "com.microsoft.word.doc",
+    "org.openxmlformats.wordprocessingml.document",
+    "org.oasis-open.opendocument.text",
+
+    "com.microsoft.excel.xls",
+    "org.openxmlformats.spreadsheetml.sheet",
+    "org.oasis-open.opendocument.spreadsheet",
+
+    "com.microsoft.powerpoint.ppt",
+    "org.openxmlformats.presentationml.presentation",
+    "org.oasis-open.opendocument.presentation",
+]
